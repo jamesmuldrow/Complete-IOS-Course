3fcefd69677b4c5624be9a5331df1b11db6d01af
James Muldrow <jamesmuldrow@gmail.com>
2018-04-08 08:28:36 -0400

    Initial Commit

--- TwitDA/TwitDA/Controllers/ViewController.swift
+++ TwitDA/TwitDA/Controllers/ViewController.swift
@@ -7,18 +7,80 @@
 //
 
 import UIKit
+import TwitterKit
+import SwiftyJSON
+import FacebookCore
+ 
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet var mainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
+//        let params = [
+//            "status":"@cytelligenceapp - First automated tweet from app!"
+//            ]
+//        //let urlString = "https://api.twitter.com/1.1/users/show.json"
+//        let urlString = "https://api.twitter.com/1.1/statuses/update.json"
+//
+//        var clientError : NSError?
+//
+//
+//
+//
+//        TWTRTwitter.sharedInstance().logIn(with: self, completion: {(session, error) -> Void in
+//            if session != nil {
+//                print("Signed in as \(String(describing: session?.userName))")
+//                self.mainLabel.text = "Successfull Login!"
+//                if let userID = TWTRTwitter.sharedInstance().sessionStore.session()?.userID {
+//                    let client = TWTRAPIClient(userID: userID)
+//                    let request = client.urlRequest(withMethod: "POST", urlString: urlString, parameters: params, error: &clientError)
+//
+//                    client.sendTwitterRequest(request, completion: {(response, data, error) in
+//                        if (error != nil){
+//                            print("Error: \(error!.localizedDescription)")
+//                            print(error!)
+//                            print(TWTRTwitter.sharedInstance().sessionStore.existingUserSessions())
+//                        } else {
+//                            let json = JSON(data!)
+//                            print(json)
+//                        }
+//                    })
+//                }
+//
+//
+//            } else {
+//                print("Error: \(String(describing: error?.localizedDescription))")
+//                print(error!)
+//            }
+//        })
+//
+//        print(TWTRTwitter.sharedInstance().version)
+        let connection = GraphRequestConnection()
+        connection.add(GraphRequest(graphPath: "/me")) { httpResponse, result in
+            switch result {
+            case .success(let response):
+                print("Graph Request Succeeded: \(response)")
+            case .failed(let error):
+                print("Graph Request Failed: \(error)")
+            }
+        }
+        connection.start()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
--- TwitDA/TwitDA/Dependancies/AppDelegate.swift
+++ TwitDA/TwitDA/Dependancies/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import TwitterKit
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        TWTRTwitter.sharedInstance().start(withConsumerKey: "df2lKxY3O1raggAOpdDXb91VO", consumerSecret: "BGikzDiHlENkEPvg2WJd9LaUvkfpOGd6a6b8ZRmjz9Tp8jIghw")
+        
+        
         return true
     }
 
@@ -39,6 +44,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+       
+        return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
     }
 
 
